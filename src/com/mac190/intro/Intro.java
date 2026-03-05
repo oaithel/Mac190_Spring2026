1a5387bcd34695735b5cd80cb0604706afa60a49
ProfOmar <oaithel@yahoo.com>
2026-03-05 13:40:39 -0500

    3/5/2026

---
+++ src/com/mac190/intro/Intro.java
@@ -0,0 +1,25 @@
+package com.mac190.intro;
+
+public class Intro {
+    public static void main(String[] args){
+        System.out.println("Welcome to MAC190, it's gonna be fun!!");
+        System.out.print("This is going to be followed ");
+        System.out.print("by this!! Then ");
+        System.out.println("this ");
+        System.out.println("But not his");
+        //Question for you: Using print, display a Diamond shape using stars like this
+        /*
+                *
+             *     *
+           *         *
+             *     *
+                *
+         */
+        System.out.println("    *");
+        System.out.println("  *    *");
+        System.out.println("*        *" );
+        System.out.println("  *    *");
+        System.out.println("     * ");
+
+    }
+}
