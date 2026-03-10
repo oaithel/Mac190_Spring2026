452abcb96e5458739be410af7ef3fa816b5af49d
ProfOmar <oaithel@yahoo.com>
2026-03-10 14:58:33 -0400

    3/10/2026

---
+++ src/com/mac190/selection/Temperature.java
@@ -0,0 +1,12 @@
+package com.mac190.selection;
+/*
+Write a program that converts Celcius to Fahrenheit and vice-versa.
+1) ask the user which conversion he/she wishes to perform: For instance
+enter 1 to convert from Fahrenheit to Celcius
+enter 2 to convert from Celcius to fahrenheit
+
+If choice is 1, then compute the celcius = fahren - 32 * 5/9  display it
+If choice is 2, then compute the fahren = celcius *9/5 + 32 display it
+ */
+public class Temperature {
+}
