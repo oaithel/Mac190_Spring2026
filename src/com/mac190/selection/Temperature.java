ab82b3883fecec3aea886444cbdad04281c1cc3f
ProfOmar <oaithel@yahoo.com>
2026-03-12 13:26:18 -0400

    3/12/2026

--- src/com/mac190/selection/Temperature.java
+++ src/com/mac190/selection/Temperature.java
@@ -1,4 +1,8 @@
 package com.mac190.selection;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 /*
 Write a program that converts Celcius to Fahrenheit and vice-versa.
 1) ask the user which conversion he/she wishes to perform: For instance
@@ -9,4 +13,25 @@ If choice is 1, then compute the celcius = fahren - 32 * 5/9  display it
 If choice is 2, then compute the fahren = celcius *9/5 + 32 display it
  */
 public class Temperature {
+    public static void main(String[] args) {
+        System.out.println("Press C to convert into Fahrenheit\n " +
+                "Press F to convert into Celcius");
+        Scanner keyb = new Scanner(System.in);
+        char choice = keyb.next().charAt(0); //The first character of the input word
+        //We have three different cases: C F or enything else. Need two if-else
+        if(Character.toLowerCase(choice) == 'c'){
+            //ask for the celcius temperature
+            System.out.println("Enter the celcius temperature: ");
+            double celcius = keyb.nextDouble();
+            double fahrenheit = celcius*9.0/5.0 + 32;
+            System.out.println(celcius + " in celcius is " + fahrenheit + " in fahrenheit");
+        }else if(Character.toLowerCase(choice) == 'f'){
+            System.out.println("Enter the fahrenheit temperature: ");
+            double fahrenheit = keyb.nextDouble();
+            double celcius = (fahrenheit - 32)*5.0/9.0;
+            System.out.println(fahrenheit + " in fahenheit is " + celcius + " in celcius");
+        }else{
+            System.out.println("Invalid choice");
+        }
+    }
 }
