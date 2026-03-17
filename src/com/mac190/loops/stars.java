de61e6caf5532dd62beed5286508029e84ea1cd9
momo <oaithel@yahoo.com>
2026-03-17 14:25:11 -0400

    3/17/2026

---
+++ src/com/mac190/loops/stars.java
@@ -0,0 +1,43 @@
+package com.mac190.loops;
+/*
+TODO: HW4
+Write a program that reads an integer from the user for the number of lines to display.
+The displays that number of lines with each line having that number of stars.
+Enter the number of lines: 5
+*****
+*****
+*****
+*****
+*****
+
+
+Once that is done. Modify the program to display the following shape:
+If input is 5
+
+*
+**
+***
+****
+*****
+
+One loop that goes through the lines call it i loop, then inside one loop
+that displays the number of stars for that line.
+i  (line number)        j(stars)  How many stars to display at line i = i+1
+0                       1   j loop should loop i+1 times.
+1                       2
+2                       3
+3                       4
+4                       5
+i loop loops the number of lines times
+
+ */
+public class stars {
+    public static void main(String[] args) {
+        //Read a number from the user into lineNumber
+
+        //loop lineNumber times
+            //Inside the loop, loop again lineNumber times
+                //display one star at a time
+            //go to next line to display the next line.
+    }
+}
