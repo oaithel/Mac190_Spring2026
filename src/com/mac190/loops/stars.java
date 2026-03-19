01c64f9e5ed854f6a7471288dfc8a2bdfdb3d2b5
momo <oaithel@yahoo.com>
2026-03-19 13:15:44 -0400

    3/17/2026

--- src/com/mac190/loops/stars.java
+++ src/com/mac190/loops/stars.java
@@ -1,4 +1,7 @@
 package com.mac190.loops;
+
+import java.util.Scanner;
+
 /*
 TODO: HW4
 Write a program that reads an integer from the user for the number of lines to display.
@@ -30,14 +33,39 @@ i  (line number)        j(stars)  How many stars to display at line i = i+1
 4                       5
 i loop loops the number of lines times
 
+Shape 2:
+*****
+****
+***
+**
+*
+
  */
 public class stars {
     public static void main(String[] args) {
         //Read a number from the user into lineNumber
-
+        Scanner  sc = new Scanner(System.in);
+        System.out.println("enter the number of lines: ");
+        int nLines = sc.nextInt();
         //loop lineNumber times
+        for(int i = 0; i < nLines; i++) {
             //Inside the loop, loop again lineNumber times
+            for(int j = 0; j < nLines; j++) {
                 //display one star at a time
+                System.out.print("*");
+            }
             //go to next line to display the next line.
+            System.out.println("");
+        }
+        System.out.println("-----------Triangle Shape 1-----");
+        for(int i = 0; i < nLines; i++) {
+            //Inside the loop, loop again lineNumber times
+            for(int j = 0; j < i+1; j++) {
+                //display one star at a time
+                System.out.print("*");
+            }
+            //go to next line to display the next line.
+            System.out.println("");
+        }
     }
 }
