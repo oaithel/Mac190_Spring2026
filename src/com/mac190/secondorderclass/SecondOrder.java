eafc2501b4cebdfdfa7460cdef9b07cbb3d3b25d
momo <oaithel@yahoo.com>
2026-03-24 14:42:44 -0400

    3/24/2026

--- src/com/mac190/secondorderclass/SecondOrder.java
+++ src/com/mac190/secondorderclass/SecondOrder.java
@@ -2,7 +2,35 @@ package com.mac190.secondorderclass;
 
 public class SecondOrder {
     //coefficients as member variables
-
+    double a, b, c;
 
     //method solve that solves the equation
+    public void solve(){
+        if(a == 0){
+            if(b == 0){
+                if(c == 0){
+                    System.out.println("The equation has infinite solution (0=0)");
+                }else{
+                    System.out.println("No solution (C = 0)");
+                }
+            }else{
+                double x = -c/b;
+                System.out.println("this is first order equation, solution: " + x);
+            }
+        }else{
+            //compute the discriminant
+            double D = Math.pow(b, 2) - 4*a*c;
+            if(D == 0) {
+                double X = -b/(2*a);
+                System.out.println("There is one double solution: " + X);
+
+            }else if(D > 0) {
+                double X1 = (-b + Math.sqrt(D))/(2*a);
+                double X2 = (-b - Math.sqrt(D))/(2*a);
+                System.out.println("There are two distinct solutions: X1 = " + X1 + " X2 = " + X2);
+            }else{
+                System.out.println("there no real solutions.");
+            }
+        }
+    }
 }
--- src/com/mac190/secondorderclass/SecondOrderTester.java
+++ src/com/mac190/secondorderclass/SecondOrderTester.java
@@ -1,8 +1,17 @@
 package com.mac190.secondorderclass;
 
+import java.util.Scanner;
+
 public class SecondOrderTester {
     public static void main(String[] args) {
         //copy main from SecondOrderEquation code and change it to
         //use a SecondOrder object
+        SecondOrder sec  = new SecondOrder();
+        Scanner keyb = new Scanner(System.in);
+        System.out.println("enter the coefficients of the equation: ");
+        sec.a = keyb.nextDouble();
+        sec.b = keyb.nextDouble();
+        sec.c = keyb.nextDouble();
+        sec.solve();
     }
 }
