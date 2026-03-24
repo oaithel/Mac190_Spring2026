c4892398f12e736e321ef6b663845c2c3d95f3a6
momo <oaithel@yahoo.com>
2026-03-24 14:13:12 -0400

    3/24/2026

---
+++ src/com/mac190/secondorderclass/SecondOrder.java
@@ -0,0 +1,8 @@
+package com.mac190.secondorderclass;
+
+public class SecondOrder {
+    //coefficients as member variables
+
+
+    //method solve that solves the equation
+}
---
+++ src/com/mac190/secondorderclass/SecondOrderTester.java
@@ -0,0 +1,8 @@
+package com.mac190.secondorderclass;
+
+public class SecondOrderTester {
+    public static void main(String[] args) {
+        //copy main from SecondOrderEquation code and change it to
+        //use a SecondOrder object
+    }
+}
