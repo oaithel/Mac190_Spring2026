ff0cd8cca89a64ca092933bf82df2e2c168bfbf8
momo <oaithel@yahoo.com>
2026-03-24 14:46:46 -0400

    3/24/2026

---
+++ src/com/mac190/calculator/CalcTester.java
@@ -0,0 +1,14 @@
+package com.mac190.calculator;
+
+public class CalcTester {
+    public static void main(String[] args) {
+        //Make this code run for as long as the user wishes to continue.
+
+        //Create a Calculator object
+        //read a string in the form A + B (with spaces between the numbers and the operator)
+        //assign the operands and the operator to the object
+        //invoke calculate
+
+        //ask the use if he/she wishes to continue.
+    }
+}
---
+++ src/com/mac190/calculator/Calculator.java
@@ -0,0 +1,8 @@
+package com.mac190.calculator;
+
+public class Calculator {
+    //member variables: two double operands and an operator (character)
+
+    //method calculate:
+    //if the operator is + display the sum, if * display the product etc...
+}
