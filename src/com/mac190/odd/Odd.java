a2cb8c993f8dda8e5d60cdbbc413e9ca6e471949
momo <oaithel@yahoo.com>
2026-03-26 13:40:42 -0400

    3/26/2026

---
+++ src/com/mac190/odd/Odd.java
@@ -0,0 +1,13 @@
+package com.mac190.odd;
+/*
+Design an class Odd that has the following properties:
+- One integer member variable num that is odd at all times.
+- getter to return the value of num
+- setter that accepts a value and sets num to that value if it's odd otherwise it sets
+it to the next odd.
+- toString that returns the object in the form "num: value"
+- method int add(int a) that returns the sum of num and a
+- method int add(Odd d) that returns the sum of num and the num of d
+ */
+public class Odd {
+}
---
+++ src/com/mac190/odd/OddTester.java
@@ -0,0 +1,14 @@
+package com.mac190.odd;
+
+public class OddTester {
+    public static void main(String[] args) {
+        //Create an Odd object od1, assign 4 to num.
+        //display it.
+
+        //create another Odd object od2, assign to it 7
+
+        //using add method show od1 added to it 6
+
+        //using add method show result of adding ad1 and od2 objects.
+    }
+}
