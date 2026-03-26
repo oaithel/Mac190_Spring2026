afe38a9c272768013a2a6fba6cb1ce26ab4001bd
momo <oaithel@yahoo.com>
2026-03-26 13:18:02 -0400

    3/26/2026

--- src/com/mac190/calculator/CalcTester.java
+++ src/com/mac190/calculator/CalcTester.java
@@ -1,14 +1,26 @@
 package com.mac190.calculator;
 
+import java.util.Scanner;
+
 public class CalcTester {
     public static void main(String[] args) {
         //Make this code run for as long as the user wishes to continue.
-
-        //Create a Calculator object
-        //read a string in the form A + B (with spaces between the numbers and the operator)
-        //assign the operands and the operator to the object
-        //invoke calculate
-
-        //ask the use if he/she wishes to continue.
+        char answer = 'y';
+        do {
+            //Create a Calculator object
+            Calculator calc = new Calculator();
+            //read a string in the form A + B (with spaces between the numbers and the operator)
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter an expression in the form A + B: ");
+            //assign the operands and the operator to the object
+            calc.first = sc.nextDouble();
+            calc.operator = sc.next().charAt(0);
+            calc.second = sc.nextDouble();
+            //invoke calculate
+            calc.calculate();
+            //ask the use if he/she wishes to continue.
+            System.out.println("Do you want to continue? [y/n]");
+            answer = sc.next().charAt(0);
+        }while(Character.toLowerCase(answer) != 'n');
     }
 }
--- src/com/mac190/calculator/Calculator.java
+++ src/com/mac190/calculator/Calculator.java
@@ -1,8 +1,32 @@
 package com.mac190.calculator;
-
+//TODO: HW6
 public class Calculator {
     //member variables: two double operands and an operator (character)
+    double first, second;
+    char operator;
 
     //method calculate:
     //if the operator is + display the sum, if * display the product etc...
+    public void calculate(){
+        switch(operator){
+            case '+':
+                System.out.println(first + " + " + second + " = " + (first + second));
+                break;
+            case '-':
+                System.out.println(first + " - " + second + " = " + (first - second));
+                break;
+            case '*':
+                System.out.println(first + " * " + second + " = " + (first * second));
+                break;
+            case '/':
+                if(second == 0){
+                    System.out.println("Division by zero!!");
+                }else {
+                    System.out.println(first + " / " + second + " = " + (first / second));
+                }
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
 }
