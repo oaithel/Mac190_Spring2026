1edb7cc56e343a82b33a3e1abffa0643648280e4
ProfOmar <oaithel@yahoo.com>
2026-03-31 14:15:20 -0400

    3/31/2026

---
+++ src/com/mac190/prime/Prime.java
@@ -0,0 +1,33 @@
+package com.mac190.prime;
+/*
+Design class Prime that has the following properties:
+- a private integer memeber variable prime that is prime at all times
+- a default constructor that a sets prime to 2
+- A constructor that accepts an integer input and sets prime
+to the closed prime number to the input. ex .if input is 4 prime is set to 5
+(use isPrime method to find the closed prime)
+- getter
+- setter sets prime to the closest prime to the input
+- toString() return the object a s a string
+- method public Prime add(int a) returns a Prime object that is the sum of this and a
+- method public Prime add(Prime p) returns a Prime object that is the sum of this and p
+- method static Prime add(Prime p1, Prime p2) returns a Prime object that is the
+sum of p1 and p2, here this does not exist because it's static
+- method static Prime add(Prime p1, int a) returns a Prime object that is the sum
+of p1 and a. No this here because it's a static method
+- test your class using all the methods.
+
+ */
+public class Prime {
+
+    static boolean isPrime(int a){
+        //return true if a is prime and false if not
+        //if a is less than 2 then return false
+        //if a is 2 then return true
+        //if a is even return false
+        //have a for loop going from 3 to sqrt(a) inclusive by steps of 2
+            //if a is divisible by i, then return false
+
+        //at the end of the loop return true.
+    }
+}
---
+++ src/com/mac190/prime/PrimeTester.java
@@ -0,0 +1,16 @@
+package com.mac190.prime;
+
+public class PrimeTester {
+    public static void main(String[] args) {
+        //create a default Prime P1, set it to 4
+        //display it.
+        //create a Prime P2 with 8 as input
+        //display it.
+        //create a Prime P3 as the sum of P1 and P2 using member of the class method add
+        //display it
+        //create a Prime P4 as the sum of P1 and P2 using the static method
+        //display it.
+        //create a Prime P5 as (P1 + 6 + P2) using only the static methods and
+        //one line code.
+    }
+}
