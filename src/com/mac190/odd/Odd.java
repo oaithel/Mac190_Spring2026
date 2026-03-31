b14c7d31ba6965ef81c59ea73619bafe4fb53d09
ProfOmar <oaithel@yahoo.com>
2026-03-31 13:32:49 -0400

    3/12/2026

--- src/com/mac190/odd/Odd.java
+++ src/com/mac190/odd/Odd.java
@@ -10,4 +10,54 @@ it to the next odd.
 - method int add(Odd d) that returns the sum of num and the num of d
  */
 public class Odd {
+    private int num =1;
+
+    //A constructor is a method without a return type that allows
+    //to construct objects with initial attributes/values
+    //It's a public method with the same name as the class
+    //The default constructor is the constructor that does not has any input paremater
+    public Odd(){
+        num = 1;
+    }
+    public Odd(int n){
+        this.setNum(n);
+    }
+    public int getNum(){
+        return num;
+    }
+    public void setNum(int n){
+        if(n % 2 == 0){
+            n++;
+        }
+        //n is odd here
+        num = n;
+    }
+    public String toString(){
+        return "num: " + num;
+    }
+    /*
+    Overlading is using the same name for two different methods. In this case
+    the two add methods are different but they have the same name. You can do that
+    as long as the type of the input parameters is different or the number of
+    parameters is different. You cannot overload solely on the return type.
+     */
+    public int add(int a){
+        return a + num;
+    }
+    //public int add(int a) is not allowed because the difference is only the return typ
+    public int add(Odd a){
+        return a.num + num;
+    }
+
+    //design a method sum that returns an Odd Object that is a sum of this object
+    //and the input Odd object
+    public Odd sum(Odd input){
+        //We have three Odd objects: input, this and the returned object as the sum
+        Odd result = new Odd(this.num + input.num);
+        return result;
+    }
+    public Odd sum(int a){
+
+    }
 }
+
--- src/com/mac190/odd/OddTester.java
+++ src/com/mac190/odd/OddTester.java
@@ -4,12 +4,27 @@ package com.mac190.odd;
 public class OddTester {
     public static void main(String[] args) {
         //Create an Odd object od1, assign 4 to num.
+        Odd od1 = new Odd();
+        od1.setNum(4);
         //display it.
-
+        System.out.println("O1: " + od1);
         //create another Odd object od2, assign to it 7
-
+        Odd od2 = new Odd();
+        od2.setNum(7);
+        System.out.println("Od2: " + od2);
         //using add method show od1 added to it 6
-
+        int sum = od1.add(6);
+        System.out.println("sum: " + sum);//11
         //using add method show result of adding ad1 and od2 objects.
+        sum = od1.add(od2);
+        System.out.println("sum: " + sum);//12
+        //Create a new Odd object with value 8 right at the beginning without using
+        //the setter
+        Odd od3 = new Odd(8);
+        System.out.println("od3: " + od3);
+        //Use sum method to add od1 and od2 to crate od4
+        Odd od4 = od1.sum(od2);
+        System.out.println("Od4: " + od4);
+
     }
 }
