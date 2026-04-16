daddd59e32e05dd4d9fa2ad33862becc4adf0455
ProfOmar <oaithel@yahoo.com>
2026-04-16 13:44:24 -0400

    4/16/2026

---
+++ src/com/mac190/minmax/MinMax.java
@@ -0,0 +1,30 @@
+package com.mac190.minmax;
+/*
+Design an implement a class MinMax that has the following properties:
+1- Two integer mebers variables: min and max where max should always be larger
+or equal than min.
+2- setter for max: if you try to set max to a value smaller than min, then
+max is set to min and min is set to that value, otherwise max is set
+to the input.
+setter setMin: if you try to set min to a value larger than max, then
+min is set to max and max is set to that value, otherwise min is set
+to the input
+-getter for max and min
+setMinMax(int a, int b) max is set to the larger value and min to the smallest
+- toString returns min and max as a coule between parenthesis: (2, 5)
+- public boolean equals(Object o)
+- method MinMax add(int a) returns a MinMax object similar to this augmented
+by a (a is added to both min and max)
+- method MinMax add(MinMax m) returns a MinMax object that is the
+sum of this and m, mins are added and maxs are added as well.
+- implement both as static methods.
+- method MinMax multiply(int a) returns a MinMax object multiplied by a
+if a is negative then min and max should be swapped (pay attention to
+the sign of a).
+- Test your class.
+
+
+ */
+public class MinMax {
+
+}
