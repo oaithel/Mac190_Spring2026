c56d39d7fb2f69d5062a03d9695ad15fb6ccbf8c
ProfOmar <oaithel@yahoo.com>
2026-04-28 13:29:47 -0400

    4/28/2026

---
+++ src/com/mac190/midtermreview2/ICube.java
@@ -0,0 +1,7 @@
+package com.mac190.midtermreview2;
+/*
+Implement the Cube class using inheritance, knowing that every Cube
+IS a Rectangle in addition it has a height.
+ */
+public class ICube {
+}
