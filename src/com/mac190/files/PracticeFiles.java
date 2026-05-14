193b861d2ac138ff32bd841de5bbdff8a909ae6d
ProfOmar <oaithel@yahoo.com>
2026-05-14 13:31:08 -0400

    5/7/2026

---
+++ src/com/mac190/files/PracticeFiles.java
@@ -0,0 +1,23 @@
+package com.mac190.files;
+/*
+Using notepad create a file data.txt where the data in the file
+has the following form:
+12313  john  Second  23.4
+33434  moe   thatOne 10.45
+34535  Lee   Ping    12.5
+....
+
+
+Write a java program in which you request the input file name.
+Create a new file with the same name with _out added to it.
+For instance if the name is data.txt, then the output file
+will be data_out.txt
+The data in the ouput file will be the same as in the input file
+except that it organized in the following way:
+
+F:john  L:Second    T:23.4  S:12313
+F:moe   L:thatOne   T:10.45 S:33434
+F:Lee   L:Ping  T:12.5  S:34535
+ */
+public class PracticeFiles {
+}
