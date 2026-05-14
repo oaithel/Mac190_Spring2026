1018dea7c5d8c353623f7309b233fe2497ff48ed
ProfOmar <oaithel@yahoo.com>
2026-05-14 14:49:19 -0400

    5/7/2026

---
+++ src/com/mac190/files/filesHW.java
@@ -0,0 +1,11 @@
+package com.mac190.files;
+/*
+You have a file in a csv form, spx.csv of the S&P 500 closes with
+the dates. You need to create a file of the 20 day moving averages.
+You start at day 20 and compute the average of the previous 20 days
+including today.
+Do the same for next days. Store the new data in a csv form
+in a new file call it SPX_20Average.csv
+ */
+public class filesHW {
+}
