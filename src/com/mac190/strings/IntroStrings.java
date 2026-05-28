bc9ee7dd1ad4a9e04ca56ad6585dfd9dee126193
momo <oaithel@yahoo.com>
2026-05-28 15:08:15 -0400

    5/26/2026

--- src/com/mac190/strings/IntroStrings.java
+++ src/com/mac190/strings/IntroStrings.java
@@ -66,9 +66,13 @@ public class IntroStrings {
 
         /*
         Example: input:
-        Hi there. how are you? are you ok? tel me about it. have fun.
+        Hi there.    how are you? are you ok? tel me about it. have fun.
         Out put should be
         Hi there. How are you? Are you ok? Tel me about it. Have fun.
          */
+        if(Character.isLetter(str2.charAt(index))){
+            str2 = str2.substring(0, index) + Character.toUpperCase(str2.charAt(index)) +
+                    str2.substring(index+1);
+        }
     }
 }
