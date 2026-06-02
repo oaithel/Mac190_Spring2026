66050a7ab0a88626e9df0aad7020d53d6cf2ebb8
ProfOmar <oaithel@yahoo.com>
2026-06-02 14:58:52 -0400

    6/2/2026

--- src/com/mac190/triangle/Shape.java
+++ src/com/mac190/triangle/Shape.java
@@ -1,11 +1,49 @@
 package com.mac190.triangle;
+
+import java.util.Objects;
+
 /*
 Design an abstact class shape that forces derived classes to implement
 the following methods and member variables:
-- doule height
+- double height
 - getter and setter for heigh, throw checked exception if input is negative
 - area as method
 - Shape add (Shape) //add method that adds two shapes.
  */
-public class Shape {
+abstract class Shape {
+    private double height;
+    public Shape(){
+        height = 0;
+    }
+    public Shape(double a){
+        height = Math.abs(a);
+    }
+    public Shape(Shape s){
+        height = s.getHeight();
+    }
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) throws Exception{
+        if(height < 0){
+            throw new Exception("height cannot be negative");
+        }
+        this.height = height;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Shape shape)) return false;
+        return Double.compare(height, shape.height) == 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "height=" + height +
+                '}';
+    }
+    public abstract double area();
+    public abstract Shape add(Shape s);
 }
--- src/com/mac190/triangle/Triangle.java
+++ src/com/mac190/triangle/Triangle.java
@@ -1,4 +1,7 @@
 package com.mac190.triangle;
+
+import java.util.Objects;
+
 /*
 Design and code a class Triangle that has the following properties:
 - A Triangle is a shape, inherit from Shape class.
@@ -17,5 +20,69 @@ height as th average of the two heights.
 - implement the method add above as a static method.
 - implement area as the base multiplied by the height
  */
-public class Triangle {
+public class Triangle extends Shape implements Addable{
+    private double base;
+    public Triangle(){
+        super();
+        base = 0;
+    }
+    public Triangle(double a){
+        super(a);
+        base = a;
+    }
+    public Triangle(double a, double b){
+        super(b);
+        base = a;
+    }
+    public Triangle (Triangle t){
+        super(t.getHeight());
+        base = t.base;
+    }
+    public double getBase() {
+        return base;
+    }
+    public void setBase(double base) throws Exception{
+        if(base < 0){
+            throw new Exception("base cnnot be negative");
+        }
+        this.base = base;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "base=" + base +
+                "} " + super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Triangle triangle)) return false;
+        if (!super.equals(o)) return false;
+        return Double.compare(base, triangle.base) == 0;
+    }
+
+    @Override
+    public Addable add(Addable a) {
+        if(! (a instanceof Triangle t)){
+            throw new ClassCastException("input needs to be a Triangle");
+        }
+        return Triangle.add(this, t);
+    }
+
+    @Override
+    public double area() {
+        return base*this.getHeight();
+    }
+
+    @Override
+    public Shape add(Shape s) {
+        if(!(s instanceof Triangle t)){
+            throw new ClassCastException("input is not a Triangle");
+        }
+        return Triangle.add(this, t);
+    }
+    public static Triangle add(Triangle t, Triangle s){
+        return new Triangle(t.base + s.getBase(), (t.getHeight() + s.getHeight())/2.0);
+    }
 }
